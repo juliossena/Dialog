2c35c75d8ead15b84c59c41c6d8e23e50e681650
juliossena <juliosouzasena@gmail.com>
2018-06-23 20:43:09 -0300

    final

--- app/src/main/java/dialog/cursoandroid/dell/dialog/MainActivity.java
+++ app/src/main/java/dialog/cursoandroid/dell/dialog/MainActivity.java
@@ -1,13 +1,55 @@
 package dialog.cursoandroid.dell.dialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnIniciar;
+    private AlertDialog.Builder dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciar = (Button) findViewById(R.id.botao_id);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new AlertDialog.Builder(MainActivity.this);
+
+                dialog.setTitle("Qual a pergunta?");
+
+                dialog.setMessage("Essa mensagem aqui");
+
+                dialog.setCancelable(false);
+                dialog.setIcon(android.R.drawable.ic_delete);
+
+                dialog.setNegativeButton("Não",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "Colocou não", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                dialog.setPositiveButton("Sim",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "Colocou sim", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                dialog.create();
+                dialog.show();
+            }
+        });
     }
 }
